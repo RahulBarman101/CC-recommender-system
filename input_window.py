40bb3ebf82224a83fa78e771c463d256def55389
Rahul Barman <rahulbarman1215@gmail.com>
2020-07-09 14:04:21 +0530

    created window and inputs for user data

--- input_window.py
+++ input_window.py
@@ -1,8 +1,38 @@
 from tkinter import *
+from functools import partial
 
 window = Tk()
 window.geometry('500x450')
 window.title('Employee Interest Input')
 
+uname = StringVar()
+domain = StringVar()
+event1 = StringVar()
+event2 = StringVar()
+
+i = 0
+
+def submit(uname,domain,event1,event2):
+	print(uname.get())
+	print(domain.get())
+	return
+
+def get_label_entry(t,tv,i):
+
+	label = Label(window,text=t,height=3,width=10).grid(column=0,row=i)
+	entry = Entry(window,textvariable=tv).grid(column=1,row=i)
+	return label,entry
+
+uname_label,uname_entry = get_label_entry('Name',uname,i)
+i+=1
+domain_label,domain_entry = get_label_entry('Domain',domain,i)
+i+=1
+event1_label,event1_entry = get_label_entry('Event1',event1,i)
+i+=1
+event2_label,event2_entry = get_label_entry('Event2',event2,i)
+
+submit = partial(submit,uname, domain, event1, event2)
+
+submit = Button(window,text='Submit',command=submit).grid(column=1,row=i+1)
 
 window.mainloop()
