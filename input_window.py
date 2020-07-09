4a0527e5ad407819bfb2a6dbcf77055d58d4bd8c
Rahul Barman <rahulbarman1215@gmail.com>
2020-07-09 23:58:35 +0530

    structured and coments added

--- input_window.py
+++ input_window.py
@@ -2,11 +2,13 @@ from tkinter import *
 from functools import partial
 from sql_functions import sql_func
 
+### initializations and initial setups
 sq = sql_func()
 window = Tk()
 window.geometry('500x450')
 window.title('Employee Interest Input')
 
+### string object variables to store the text data
 uname = StringVar()
 domain = StringVar()
 event1 = StringVar()
@@ -15,15 +17,26 @@ event2 = StringVar()
 i = 0
 
 def submit(uname,domain,event1,event2):
+	''' 
+	function for button press and store of data
+	'''
 	sq.insert_data(uname.get(),domain.get(),event1.get(),event2.get())
 	sq.close()
 	return
 
 def get_label_entry(t,tv,i):
-
+	'''
+	function to create lable and entries widget for the tkinter window
+	params:
+	t-> text of the label
+	tv-> the string variable object to store the user typed data
+	i-> to keep track of the row to place the widgets
+	'''
 	label = Label(window,text=t,height=3,width=10).grid(column=0,row=i)
 	entry = Entry(window,textvariable=tv).grid(column=1,row=i)
 	return label,entry
+
+
 
 uname_label,uname_entry = get_label_entry('Name',uname,i)
 i+=1
