9699662e4a99990708da77ac7a13624aa2fc10a3
Rahul Barman <rahulbarman1215@gmail.com>
2020-07-12 13:28:49 +0530

    changed sql functions and changed to class oriented

--- input_window.py
+++ input_window.py
@@ -1,72 +1,71 @@
 from tkinter import *
+from tkinter import messagebox as msgb
 from functools import partial
 from sql_functions import sql_func
-import pickle
 
-### initializations and initial setups
-sq = sql_func()
-window = Tk()
-window.geometry('500x450')
-window.title('Employee Interest Input')
+class input_win:
+	def __init__(self):
+		self.sq = sql_func()
+		self.window = Tk()
+		self.window.geometry('500x450')
+		self.window.title('Employee Interest Input')
 
+		self.uname = StringVar()
+		self.domain = StringVar()
+		self.event1 = StringVar()
+		self.event2 = StringVar()
 
-### string object variables to store the text data
-uname = StringVar()
-domain = StringVar()
-event1 = StringVar()
-event2 = StringVar()
+		self.new_win = None
+		uname_label = Label(self.window,text='Name',height=3,width=10).grid(column=0,row=1)
+		uname_entry = Entry(self.window,textvariable=self.uname,width=30).grid(column=1,row=1,padx=20)
 
-global new_win
+		domain_label = Label(self.window,text='Domain').grid(column=0,row=2)
+		self.domain.set('Artificial Intelligence')
+		domain_drop = OptionMenu(self.window, self.domain, "Artificial Intelligence", "Blockchain", "C",\
+		 "C++", "Cloud Computing", "Coding", "Data Science", "Development processes",\
+		  "Finance", "Hardware", "Higher Education", "IoT", "Java", "JavaScript",\
+		   "Machine learning", "Management", "Mobile Applications", "Networking",\
+		    "Python", "Security", "Software Architecture", "Web Development", "Other")
+		domain_drop.grid(column=1,row=2)
 
-def submit(uname,domain,event1,event2):
-	''' 
-	function for button press and store of data
-	'''
-	MsgBox = messagebox.askquestion ('Submitted','Submitted and stored successfully',
+		event1_label = Label(self.window,text='Event1').grid(column=0,row=3)
+		self.event1.set('Certifications')
+		event1_drop = OptionMenu(self.window, self.event1, "Certifications", "Competitions", "Courses",\
+		 "Expos", "Fests", "Hackathons", "Internships", "Jobs", "Seminars", "Talks",\
+		  "Trainings", "Webinars", "Workshops")
+		event1_drop.grid(column=1,row=3)
+
+		event2_label = Label(self.window,text='Event2').grid(column=0,row=3)
+		self.event2.set('Certifications')
+		event2_drop = OptionMenu(self.window, self.event2, "Certifications", "Competitions", "Courses",\
+		 "Expos", "Fests", "Hackathons", "Internships", "Jobs", "Seminars", "Talks",\
+		  "Trainings", "Webinars", "Workshops")
+		event2_drop.grid(column=1,row=4)
+
+		submit = partial(self.submit,self.uname, self.domain, self.event1, self.event2)
+
+		submit = Button(self.window,text='Submit',command=submit).grid(column=1,row=5)
+
+		self.window.mainloop()
+
+	def submit(self,uname,domain, event1, event2):
+		self.sq.insert_data(uname.get(),domain.get(),event1.get(),event2.get())
+		self.sq.close()
+
+		MsgBox = msgb.askquestion('Submitted','Submitted and stored successfully',
 									icon = 'warning')
-	if MsgBox == 'yes':
-		window.destroy()
-		create_new_win()
-		return 
-	else:
-		messagebox.showinfo('Return','You will now return to the screen')
-	sq.insert_data(uname.get(),domain.get(),event1.get(),event2.get())
-	sq.close()
-	return
+		if MsgBox == 'yes':
+			self.window.destroy()
+			self.create_new_win()
+			return
 
-def create_new_win():
-	new_win = Tk()
-	new_win.geometry('500x450')
-	
+		else:
+			messagebox.showinfo('Return','You will now return to the screen')
 
-uname_label = Label(window,text='Name',height=3,width=10).grid(column=0,row=1)
-uname_entry = Entry(window,textvariable=uname,width=30).grid(column=1,row=1,padx=20)
+		return
 
-domain_label = Label(window,text='Domain').grid(column=0,row=2)
-domain.set('Artificial Intelligence')
-domain_drop = OptionMenu(window, domain, "Artificial Intelligence", "Blockchain", "C",\
- "C++", "Cloud Computing", "Coding", "Data Science", "Development processes",\
-  "Finance", "Hardware", "Higher Education", "IoT", "Java", "JavaScript",\
-   "Machine learning", "Management", "Mobile Applications", "Networking",\
-    "Python", "Security", "Software Architecture", "Web Development", "Other")
-domain_drop.grid(column=1,row=2)
+	def create_new_win(self):
+		self.new_win = Tk()
+		self.new_win.geometry('500x450')
 
-event1_label = Label(window,text='Event1').grid(column=0,row=3)
-event1.set('Certifications')
-event1_drop = OptionMenu(window, event1, "Certifications", "Competitions", "Courses",\
- "Expos", "Fests", "Hackathons", "Internships", "Jobs", "Seminars", "Talks",\
-  "Trainings", "Webinars", "Workshops")
-event1_drop.grid(column=1,row=3)
-
-event2_label = Label(window,text='Event2').grid(column=0,row=3)
-event2.set('Certifications')
-event2_drop = OptionMenu(window, event2, "Certifications", "Competitions", "Courses",\
- "Expos", "Fests", "Hackathons", "Internships", "Jobs", "Seminars", "Talks",\
-  "Trainings", "Webinars", "Workshops")
-event2_drop.grid(column=1,row=4)
-
-submit = partial(submit,uname, domain, event1, event2)
-
-submit = Button(window,text='Submit',command=submit).grid(column=1,row=5)
-
-window.mainloop()
+input_win()
